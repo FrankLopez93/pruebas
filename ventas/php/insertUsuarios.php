3237ad0db699ffdae36145d3f661194d777dfd07
FrankLopez93 <francisco.lopez666@hotmail.com>
2022-05-08 12:07:06 -0500

    se agrega el campo  al INSERT

--- ventas/php/insertUsuarios.php
+++ ventas/php/insertUsuarios.php
@@ -1,5 +1,4 @@
 <?php
-
     require("conexion.php");
     $con = mysqli_connect($servidor, $usuario, $contrasena, $bd);
 
@@ -9,12 +8,13 @@
     $apMaterno = $_POST["apMaterno"];
     $email = $_POST["email"];
     $celular = $_POST["celular"];
+    $rol = $_POST["rol"];
 
-    $sql = "INSERT INTO usuarios (nombre, ap_paterno, ap_materno, email, celular)
-            VALUES('$nombre', '$apPaterno', '$apMaterno', '$email', '$celular')";
+    $sql = "INSERT INTO usuarios (nombre, ap_paterno, ap_materno, email, celular, id_rol)
+            VALUES('$nombre', '$apPaterno', '$apMaterno', '$email', '$celular', '$rol')";
     
     if (mysqli_query($con, $sql) === TRUE) {
-        echo "Registro realizado";        
+        echo "Registro realizado";
     }else {
         echo "ERROR!!!";
     }
